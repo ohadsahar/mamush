111b8ec7005ac59f7e246416488aade37162a3fd
Ohad Sahar <ohad@any-app.com>
2021-05-18 16:56:40 +0300

    Heroku

--- mamush_server/dist/api/utils/mail.transformer.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.mailTransformer = void 0;
-exports.mailTransformer = (mail) => {
-    return mail === null || mail === void 0 ? void 0 : mail.trim().toLowerCase();
-};
-//# sourceMappingURL=mail.transformer.js.map
